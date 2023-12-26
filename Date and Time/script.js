4931ca94ee38f601f0ed8a747840b7b6839196b0
Yash Vijay Bombale <yashbombale@gmail.com>
2023-12-26 14:10:32 +0530

    added date and time

---
+++ Date and Time/script.js
@@ -0,0 +1,23 @@
+// const date = new Date();
+
+// console.log(date);
+//not readable;
+
+// console.log();
+// console.log(`.toLocaleString() ==> ${date.toLocaleString()}`);
+// console.log(`.toString() ==> ${date.toString()}`);
+// console.log(`.toDateString() ==> ${date.toDateString()}`);
+// console.log(`.JSON() ==> ${date.toJSON()}`);
+// console.log(`.toLocaleDateString() ==> ${date.toLocaleDateString()}`);
+// console.log(`.toLocaleTimeString() ==>  ${date.toLocaleTimeString()}`);
+
+// const myDate = Date.now();
+// console.log(Math.floor(Date.now() / 1000));
+//converting miliseconds to seconds by dividing it by 1000 Date.now() / 1000;
+
+const newDate = new Date();
+
+console.log(newDate.toLocaleString());
+// TODO: we can customize .toLocaleString();
+
+console.log(newDate);
