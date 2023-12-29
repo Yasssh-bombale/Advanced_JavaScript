ee6c38a56ff9f14e4d851ddea0de76d2e5a07225
Yash Vijay Bombale <yashbombale@gmail.com>
2023-12-29 23:29:29 +0530

    added querySelectors and querySelctorAll

--- DOM/1_Intro/script.js
+++ DOM/1_Intro/script.js
@@ -9,3 +9,13 @@ const myli = document.querySelectorAll("li");
 // console.log(myli);
 myli[0].style.backgroundColor = "yellow";
 myli[2].style.backgroundColor = "green";
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++;
+
+const common = document.getElementsByClassName("class-1");
+const test = Array.from(common);
+// * Note:Array.from() created an array from the array like objects; here common has the HTMLCollection which is an array like objects ;
+console.log(common);
+// ? If i want to change all the list items background to yellow then i have here forEach method because i converted the HTMLCOLLECTION to the Array ;
+
+test.forEach((l) => (l.style.color = "yellow"));
